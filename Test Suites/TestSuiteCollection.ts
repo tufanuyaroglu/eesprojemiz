e7ae415cdbeb9feec105ac2980d639ce182584e8
Tufan.Uyaroglu <Tufan.Uyaroglu@ELEntTstTerm11>
2020-09-11 16:01:37 +0300

    Cuma-Part1

--- Test Suites/TestSuiteCollection.ts
+++ Test Suites/TestSuiteCollection.ts
@@ -3,7 +3,7 @@
    <description></description>
    <name>TestSuiteCollection</name>
    <tag></tag>
-   <executionMode>SEQUENTIAL</executionMode>
+   <executionMode>PARALLEL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
